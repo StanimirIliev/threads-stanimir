ddc954de4b702bf5d9373bcce5b0a50a52a224ac
Stanimir <stambeto2197@gmail.com>
2017-09-29 14:52:05 +0300

    Concurrency programming. Threads.
    Simple hastable with timeout for its nodes.
    The timeout is predefined in the constructor of the class.
    Every time a node is used (get value by key) the timer is reset.
    If the time on one node reaches 0 the node is deleted.

---
+++ src/fifthtask/TimeoutHashtable.java
@@ -0,0 +1,94 @@
+package fifthtask;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class TimeoutHashtable<K, V> {
+
+    private class Node {
+
+        private class Timeout extends Thread {
+
+            private boolean isDone = false;
+
+            public void run(){
+                isDone = false;
+                try{
+                    Thread.sleep(millis);
+                }
+                catch(InterruptedException e){
+                    return;
+                }
+                isDone = true;
+            }
+        }
+
+        private K key;
+        private V value;
+        private Timeout timeoutThread;
+
+        private Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+            timeoutThread = new Timeout();
+            timeoutThread.start();
+        }
+
+        private V get(K key){
+            timeoutThread.interrupt();
+            timeoutThread.run();
+            return value;
+        }
+
+        private void put(V value){
+            this.value = value;
+            timeoutThread.interrupt();
+            timeoutThread.run();
+        }
+    }
+
+    private List<Node> nodes;
+    final long millis;
+
+    public TimeoutHashtable(long millis) {
+        this.millis = millis;
+        nodes = new LinkedList<>();
+    }
+
+    public void put(K key, V value){
+        for(Node node : nodes){
+            if(node.get(key) != null){
+                if(node.timeoutThread.isAlive()){
+                    nodes.remove(node);
+                    nodes.add(new Node(key, value));
+                }
+                node.put(value);
+            }
+        }
+        nodes.add(new Node(key, value));
+    }
+
+    public V get(K key){
+        for(Node node : nodes){
+            if(node.key.equals(key)){
+                if(node.timeoutThread.isDone){
+                    nodes.remove(node);
+                    return null;
+                }
+                return node.get(key);
+            }
+        }
+        return null;
+    }
+
+    public V remove(K key){
+        for(int i = 0; i < nodes.size(); i++){
+            if(nodes.get(i).key.equals(key)){
+                V temp = nodes.get(i).value;
+                nodes.remove(i);
+                return temp;
+            }
+        }
+        return null;
+    }
+}

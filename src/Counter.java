6d18893ac42b61900cd99a211542898886440f1f
Stanimir <stambeto2197@gmail.com>
2017-10-09 10:40:40 +0300

    Edited task to using locks

--- src/Counter.java
+++ src/Counter.java
@@ -1,26 +1,50 @@
-public class Counter implements Runnable {
-    
-    final int finalValue;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Counter extends Thread {
+
+    private boolean locked = false;
+    private final int finalValue;
+    private final Lock lock = new ReentrantLock();
+    private Counter otherThread;
 
     public Counter(int finalValue) {
         this.finalValue = finalValue;
     }
 
-    private void threadMessage(String message){
-        System.out.printf("%s:\t%s\n", Thread.currentThread().getName(), message);
+    public void addRef(Counter otherThread){
+        this.otherThread = otherThread;
+    }
+
+    public boolean isLocked(){
+        return locked;
     }
 
     public void run(){
         try{
             for(int i = 1; i <= finalValue; i++){
-                synchronized (this){
+                Thread.sleep(1000);
+                if(otherThread.isLocked()){
+                    i--;
+                    continue;
+                }
+                try{
+                    locked = lock.tryLock();
                     threadMessage(String.valueOf(i));
                 }
-                Thread.sleep(1000);
+                finally{
+                    lock.unlock();
+                    locked = false;
+                }
             }
         }
         catch(InterruptedException e){
             threadMessage("I was interrupted and exit");
         }
     }
+
+    private void threadMessage(String message){
+        System.out.printf("%s:\t%s\n", Thread.currentThread().getName(), message);
+    }
+
 }
--- src/MainClass.java
+++ src/MainClass.java
@@ -1,9 +1,11 @@
 public class MainClass {
     public static void main(String[] args){
-        Thread t1 = new Thread(new Counter(5));
-        Thread t2 = new Thread(new Counter(5));
+        Counter t1 = new Counter(5);
+        Counter t2 = new Counter(7);
         t1.setName("Thread 1");
         t2.setName("Thread 2");
+        t1.addRef(t2);
+        t2.addRef(t1);
         t1.start();
         t2.start();
     }

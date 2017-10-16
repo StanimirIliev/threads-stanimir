f0e447def7e764ccf5c62e16710ff207c10286ca
Stanimir <stambeto2197@gmail.com>
2017-10-16 14:22:34 +0300

    Changes:
    Changed timeout to remove node right after the time expires instead of
    waiting the user to use this element

--- src/timeouthashtable/TimeoutHashtable.java
+++ src/timeouthashtable/TimeoutHashtable.java
@@ -1,93 +1,78 @@
 package timeouthashtable;
 
+import java.util.Hashtable;
 import java.util.LinkedList;
-import java.util.List;
+import java.util.Map;
+
+import static java.lang.Thread.sleep;
 
 public class TimeoutHashtable<K, V> {
 
-    private class Node {
-
-        private class Timeout extends Thread {
-
-            private boolean isDone = false;
-
-            public void run(){
-                isDone = false;
-                try{
-                    Thread.sleep(millis);
-                }
-                catch(InterruptedException e){
-                    return;
-                }
-                isDone = true;
-            }
-        }
+    private class Timeout implements Runnable {
 
         private K key;
-        private V value;
-        private Timeout timeoutThread;
 
-        private Node(K key, V value) {
+        private Timeout(K key) {
             this.key = key;
-            this.value = value;
-            timeoutThread = new Timeout();
-            timeoutThread.start();
         }
 
-        private V get(K key){
-            timeoutThread.interrupt();
-            timeoutThread.run();
-            return value;
-        }
-
-        private void put(V value){
-            this.value = value;
-            timeoutThread.interrupt();
-            timeoutThread.run();
+        @Override
+        public void run() {
+            try {
+                sleep(millis);
+                entries.remove(key);
+            } catch (InterruptedException e) {}
         }
     }
 
-    private List<Node> nodes;
+    private class TimeoutNode {
+        private V value;
+        private Thread timeout;
+
+        private TimeoutNode(V value, Thread timeout) {
+            this.value = value;
+            this.timeout = timeout;
+            timeout.start();
+        }
+
+        private void resetTimer(K key){
+            timeout.interrupt();
+            timeout = new Thread(new Timeout(key));
+            timeout.start();
+        }
+    }
+
+    private Map<K, TimeoutNode> entries = new Hashtable<>();
     final long millis;
 
     public TimeoutHashtable(long millis) {
         this.millis = millis;
-        nodes = new LinkedList<>();
     }
 
     public void put(K key, V value){
-        for(Node node : nodes){
-            if(node.get(key) != null){
-                if(node.timeoutThread.isAlive()){
-                    nodes.remove(node);
-                    nodes.add(new Node(key, value));
-                }
-                node.put(value);
-            }
-        }
-        nodes.add(new Node(key, value));
+        entries.put(key, new TimeoutNode(value, new Thread(new Timeout(key))));
     }
 
-    public V get(K key){
-        for(Node node : nodes){
-            if(node.key.equals(key)){
-                if(node.timeoutThread.isDone){
-                    nodes.remove(node);
-                    return null;
-                }
-                return node.get(key);
+    public synchronized V get(K key){
+        TimeoutNode node = entries.get(key);
+        if(node != null){
+            if(node.timeout.isAlive()){
+                node.resetTimer(key);
+                return node.value;
             }
+            entries.remove(key);
         }
         return null;
     }
 
-    public V remove(K key){
-        for(int i = 0; i < nodes.size(); i++){
-            if(nodes.get(i).key.equals(key)){
-                V temp = nodes.get(i).value;
-                nodes.remove(i);
-                return temp;
+    public synchronized V remove(K key){
+        TimeoutNode node = entries.get(key);
+        if(node != null){
+            if(node.timeout.isAlive()){
+                entries.remove(key);
+                return node.value;
             }
+            entries.remove(key);
         }
         return null;
     }
